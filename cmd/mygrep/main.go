182efd474d60974818e2a7f3f2502b933f48f617
ebembi-dream11 <87430078+ebembi-dream11@users.noreply.github.com>
2022-12-10 20:42:50 +0530

    First stage

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	// Uncomment this to pass the first stage
-	// "bytes"
+	"bytes"
 	"fmt"
 	"io"
 	"os"
@@ -48,7 +48,7 @@ func matchLine(line []byte, pattern string) (bool, error) {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this to pass the first stage
-	// ok = bytes.ContainsAny(line, pattern)
+	ok = bytes.ContainsAny(line, pattern)
 
 	return ok, nil
 }
